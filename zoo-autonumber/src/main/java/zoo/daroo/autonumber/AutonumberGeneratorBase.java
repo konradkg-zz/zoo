e19ed755acc527551b597501ee487048ec8d9cb6
daru <dariusz.szablinski@gmail.com>
2012-10-22 22:58:21 +0200

    c

--- zoo-autonumber/src/main/java/zoo/daroo/autonumber/AutonumberGeneratorBase.java
+++ zoo-autonumber/src/main/java/zoo/daroo/autonumber/AutonumberGeneratorBase.java
@@ -103,76 +103,13 @@ public abstract class AutonumberGeneratorBase<T extends Number> implements IAuto
 		}
 	}
 	
-	/**
-	 * class CachedData {
-   Object data;
-   volatile boolean cacheValid;
-   ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
-
-   void processCachedData() {
-     rwl.readLock().lock();
-     if (!cacheValid) {
-        // Must release read lock before acquiring write lock
-        rwl.readLock().unlock();
-        rwl.writeLock().lock();
-        // Recheck state because another thread might have acquired
-        //   write lock and changed state before we did.
-        if (!cacheValid) {
-          data = ...
-          cacheValid = true;
-        }
-        // Downgrade by acquiring read lock before releasing write lock
-        rwl.readLock().lock();
-        rwl.writeLock().unlock(); // Unlock write, still hold read
-     }
-
-     use(data);
-     rwl.readLock().unlock();
-   }
- }
- 
-ReentrantReadWriteLocks can be used to improve concurrency in some uses of some kinds of Collections. This is typically worthwhile only when the collections are expected to be large, accessed by more reader threads than writer threads, and entail operations with overhead that outweighs synchronization overhead. For example, here is a class using a TreeMap that is expected to be large and concurrently accessed. 
-
- class RWDictionary {
-    private final Map m = new TreeMap();
-    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
-    private final Lock r = rwl.readLock();
-    private final Lock w = rwl.writeLock();
-
-    public Data get(String key) {
-        r.lock();
-        try { return m.get(key); }
-        finally { r.unlock(); }
-    }
-    public String[] allKeys() {
-        r.lock();
-        try { return m.keySet().toArray(); }
-        finally { r.unlock(); }
-    }
-    public Data put(String key, Data value) {
-        w.lock();
-        try { return m.put(key, value); }
-        finally { w.unlock(); }
-    }
-    public void clear() {
-        w.lock();
-        try { m.clear(); }
-        finally { w.unlock(); }
-    }
- }}
-
-	 * @author daru
-	 *
-	 */
-
+	
 	/*private class AutoId {
 		private final int range;
 		private final int node;
 
 		private final AtomicLong current = new AtomicLong();
 		private final AtomicLong last = new AtomicLong(Long.MIN_VALUE);
-
-		//private final AtomicBoolean updating = new AtomicBoolean(false);
 
 		AutoId(int node, int range) {
 			this.node = node;
