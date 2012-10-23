b137b55141aa519a3ced3289689b1b9a284e3eba
d.szablinski <d.szablinski@mob4804>
2012-10-23 17:40:55 +0200

    fd

--- zoo-camel-libs/src/main/java/zoo/camel/boot/RoutesWatchDog.java
+++ zoo-camel-libs/src/main/java/zoo/camel/boot/RoutesWatchDog.java
@@ -5,7 +5,11 @@ import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import static java.nio.file.StandardWatchEventKinds.OVERFLOW;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
@@ -14,14 +18,22 @@ import java.nio.file.WatchService;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class RoutesWatchDog {
+import org.apache.camel.model.ModelCamelContext;
+import org.apache.camel.model.RoutesDefinition;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class RoutesWatchDog implements InitializingBean, DisposableBean {
 
     final Path contextDir = Paths.get("D:/temp/contexts");
     private WatchService watcher;
     private Thread watchDogTask;
 
     private AtomicBoolean started = new AtomicBoolean(false);
-
+    private final EventsListener eventsListener = new EventsListener();
+    
+    private ModelCamelContext camelContext;
+    
     public void start() throws IOException {
 	watcher = contextDir.getFileSystem().newWatchService();
 	contextDir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
@@ -46,6 +58,23 @@ public class RoutesWatchDog {
 	    }
 	}
     }
+    
+    @Override
+    public void afterPropertiesSet() throws Exception {
+	start();
+	
+    }
+    
+    @Override
+    public void destroy() throws Exception {
+	stop();
+    }
+    
+    public void setCamelContext(ModelCamelContext camelContext) {
+        this.camelContext = camelContext;
+    }
+
+
 
     private class WatchDogTask implements Runnable {
 	@Override
@@ -57,15 +86,21 @@ public class RoutesWatchDog {
 		    for (WatchEvent<?> event : events) {
 			if (event.kind() == OVERFLOW)
 			    continue;
-			
+
+			@SuppressWarnings("unchecked")
+                        final Path filename = contextDir.resolve(((WatchEvent<Path>) event).context());
 			if (event.kind() == ENTRY_CREATE) {
 			    System.out.println("Created: " + event.context().toString());
+			    eventsListener.onCreate(filename);
+			    continue;
 			}
 			if (event.kind() == ENTRY_DELETE) {
 			    System.out.println("Delete: " + event.context().toString());
+			    eventsListener.onDelete(filename);
 			}
 			if (event.kind() == ENTRY_MODIFY) {
 			    System.out.println("Modify: " + event.context().toString());
+			    eventsListener.onModify(filename);
 			}
 		    }
 		    boolean valid = watchKey.reset();
@@ -81,11 +116,36 @@ public class RoutesWatchDog {
 	    }
 	}
     }
+    
+    private class EventsListener {
+	
+	public void onCreate(Path path) {
+	    final File file = path.toFile();
+	    try (InputStream is = new FileInputStream(file)) {
+	        RoutesDefinition routes = camelContext.loadRoutesDefinition(is);
+	        camelContext.addRouteDefinitions(routes.getRoutes());
+            } catch (Exception e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+            }
+	}
+	
+	public void onDelete(Path path) {
+	    
+	}
+	
+	public void onModify(Path path) {
+	    
+	}
+	
+    }
     /*
      * InputStream is = getClass().getResourceAsStream("barRoute.xml");
      * RoutesDefinition routes = context.loadRoutesDefinition(is);
      * context.addRouteDefinitions(routes.getRoutes());
      */
+
+    
 
     // http://docs.oracle.com/javase/tutorial/essential/io/notification.html
 }
--- zoo-camel-libs/src/main/java/zoo/camel/boot/Standalone.java
+++ zoo-camel-libs/src/main/java/zoo/camel/boot/Standalone.java
@@ -5,7 +5,7 @@ import org.apache.camel.spring.Main;
 public class Standalone {
     
     private Main main;
-    private RoutesWatchDog routesWatchDog = new RoutesWatchDog();
+    //private RoutesWatchDog routesWatchDog = new RoutesWatchDog();
     
     public static void main(String[] args) throws Exception {
 	Standalone standalone = new Standalone();
@@ -16,7 +16,8 @@ public class Standalone {
 	main = new Main();
 	main.enableHangupSupport();
 	main.setApplicationContextUri("classpath:META-INF/spring/camel-context.xml");
-	routesWatchDog.start();
+	main.getCamelContexts();
+	//routesWatchDog.start();
 	System.out.println("Starting Camel. Use ctrl + c to terminate the JVM.\n");
         main.run();
     }
