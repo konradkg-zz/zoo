592aec2fad80bfc23a68bac4281a2ca8d49eefd9
daru <dariusz.szablinski@gmail.com>
2012-10-26 23:55:05 +0200

    da

--- zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/InternalDbManager.java
+++ zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/InternalDbManager.java
@@ -1,14 +1,28 @@
 package zoo.daroo.h2.mem;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.sql.DataSource;
 
-class InternalDbManager {
+import org.springframework.core.io.Resource;
+
+import zoo.daroo.h2.mem.spring.JdbcConnectionPoolBean;
+
+public class InternalDbManager {
+	public final static String BEAN_ID = "InternalDbManager";
 	
-	private final DataSource dataSource;
+	@Inject
+	@Named(JdbcConnectionPoolBean.BEAN_ID)
+	private DataSource dataSource;
 	
-	InternalDbManager(DataSource dataSource) {
-		this.dataSource = dataSource;
+	private Resource initScriptLocation;
+
+	public void initDatabase() {
+		
 	}
 	
-
+	
+	public void setInitScriptLocation(Resource initScriptLocation) {
+		this.initScriptLocation = initScriptLocation;
+	}
 }
--- zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/Server.java
+++ zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/Server.java
@@ -2,26 +2,19 @@ package zoo.daroo.h2.mem;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.core.io.Resource;
-
-import zoo.daroo.h2.mem.spring.JdbcConnectionPoolBean;
 
 public class Server implements InitializingBean, DisposableBean {
 
 	@Inject
-	@Named(JdbcConnectionPoolBean.BEAN_ID)
-	private DataSource internalDataSource;
+	@Named(InternalDbManager.BEAN_ID)
+	private InternalDbManager internalDbManager;
 	
-	private Resource initScriptLocation;
 	
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		
-		internalDataSource.getConnection();
 		
 		
 		// TODO init mem connection
@@ -36,8 +29,5 @@ public class Server implements InitializingBean, DisposableBean {
 		
 	}
 
-	public void setInitScriptLocation(Resource initScriptLocation) {
-		this.initScriptLocation = initScriptLocation;
-	}
-
+	
 }
