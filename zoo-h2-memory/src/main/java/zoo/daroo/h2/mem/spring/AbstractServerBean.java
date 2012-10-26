763ba4e953cb5a0363646ced7d159d93f2243917
daru <dariusz.szablinski@gmail.com>
2012-10-26 21:51:49 +0200

    s

--- zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/spring/AbstractServerBean.java
+++ zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/spring/AbstractServerBean.java
@@ -4,6 +4,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
@@ -15,11 +17,14 @@ public abstract class AbstractServerBean implements InitializingBean, Disposable
 	private boolean enable;
 	protected List<String> parameters = new ArrayList<String>();
 	
+	Log logger = LogFactory.getLog(getClass());
 	
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		if(enable) {
 			server = createServer(parameters.toArray(new String[0]));
+			server.setOut(System.err);
+			//logger.
 			server.start();
 		}
 	}
