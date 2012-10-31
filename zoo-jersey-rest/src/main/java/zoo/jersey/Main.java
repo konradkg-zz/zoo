dd65e0b4158dc11d0b8b6377edf9d74c9516b3da
daru <dariusz.szablinski@gmail.com>
2012-10-31 21:02:24 +0100

    x

--- zoo-jersey-rest/src/main/java/zoo/jersey/Main.java
+++ zoo-jersey-rest/src/main/java/zoo/jersey/Main.java
@@ -1,13 +1,20 @@
 package zoo.jersey;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 
 		System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-		// server.start();
+		final HttpServer httpServer = HttpServer.createSimpleServer("/test", 8080);
+		httpServer.start();
+		
+		
 		while (System.in.available() == 0) {
 			Thread.sleep(5000);
 		}
+		
+		httpServer.stop();
 	}
 }
