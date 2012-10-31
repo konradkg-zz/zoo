8ec9121bf62ad6a8960244b3b4e45e58f4ff47af
daru <dariusz.szablinski@gmail.com>
2012-10-31 21:42:06 +0100

    c

--- zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/JettyMainXml.java
+++ zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/JettyMainXml.java
@@ -1,23 +1,19 @@
 package zoo.jersey.jetty;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.xml.XmlConfiguration;
 
-
-public class Main {
+public class JettyMainXml {
 	
 	public static void main(String[] args) throws Exception {
 
 		System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-		final Server server = new Server(8080);
+		Server server = new Server();
+		XmlConfiguration configuration = new XmlConfiguration(JettyMainXml.class.getResourceAsStream("jetty.xml"));
+		configuration.configure(server);
+		server.start();
 		
-//		 WebApplicationContext bb = new WebApplicationContext();
-//	        bb.setServer(server);
-//	        bb.setContextPath("/");
-//	        bb.setWar("src/main/webapp");
 
-//	        server.setHandler(handler)
-
-		
 		server.start();
 		while (System.in.available() == 0) {
 			Thread.sleep(5000);
