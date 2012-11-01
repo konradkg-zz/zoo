4309a024b8f2ad4712b4b0cee30d1c0742750b6c
daru <dariusz.szablinski@gmail.com>
2012-11-01 19:04:44 +0100

    d

--- zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/rest/jaxb/SimpleResponse.java
+++ zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/rest/jaxb/SimpleResponse.java
@@ -1,16 +1,21 @@
 package zoo.jersey.jetty.rest.jaxb;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"text"})
 @XmlRootElement(name="resp")
 public class SimpleResponse {
 	@XmlElement
 	private String text;
 
-//	public String getText() {
-//		return text;
-//	}
+	public String getText() {
+		return text;
+	}
 
 	public void setText(String text) {
 		this.text = text;
