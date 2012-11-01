10fc5401fdcf88a283501d9aa9d7f19d59ed09de
daru <dariusz.szablinski@gmail.com>
2012-11-01 17:45:44 +0100

    s

--- zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/rest/TestResource.java
+++ zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/rest/TestResource.java
@@ -5,13 +5,30 @@ import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 import zoo.jersey.jetty.rest.beans.SimpleBean;
 
 import com.yammer.metrics.annotation.Timed;
 
 @Path("/")
 @Named
-public class TestResource {
+public class TestResource implements InitializingBean, DisposableBean {
+	private final Log logger = LogFactory.getLog(getClass()); 
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		logger.info("INIT");
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		logger.info("DESTROY");
+		
+	}
 	
 	@Inject
 	@Named("SimpleBean")
