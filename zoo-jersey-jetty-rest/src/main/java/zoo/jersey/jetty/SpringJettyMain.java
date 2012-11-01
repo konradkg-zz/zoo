0b3fda5b57a805d7da7940b78c66879776c50cf4
daru <dariusz.szablinski@gmail.com>
2012-11-01 09:38:08 +0100

    f

---
+++ zoo-jersey-jetty-rest/src/main/java/zoo/jersey/jetty/SpringJettyMain.java
@@ -0,0 +1,19 @@
+package zoo.jersey.jetty;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringJettyMain {
+	
+	public static void main(String[] args) throws Exception {
+		
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-jetty.xml");
+		
+		while (System.in.available() == 0) {
+			Thread.sleep(5000);
+		}
+		
+		applicationContext.destroy();
+		
+	}
+
+}
