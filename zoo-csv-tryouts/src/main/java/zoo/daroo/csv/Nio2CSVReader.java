2cb9bfd360db9936fa28135c5dfec803d6293ab0
daru <dariusz.szablinski@gmail.com>
2012-11-02 22:20:11 +0100

    s

--- zoo-csv-tryouts/src/main/java/zoo/daroo/csv/Nio2CSVReader.java
+++ zoo-csv-tryouts/src/main/java/zoo/daroo/csv/Nio2CSVReader.java
@@ -9,50 +9,64 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
 public class Nio2CSVReader {
 	
-	static char rowDelimiter = (char) 0x86;
-	static char columnDelimiter = (char) 0x87;
+//	static char rowDelimiter = (char) 0x86;
+//	static char columnDelimiter = (char) 0x87;
+	
+	static char rowDelimiter = '\u2020';
+	static char columnDelimiter = '\u2021';
 	
 	public static void main(String[] args) throws Exception{
+		
 		final Path path = Paths.get("p:/Temp/h2_data/pex_exchange.dat");
 		try (SeekableByteChannel seekableByteChannel = Files.newByteChannel(path,
 				EnumSet.of(StandardOpenOption.READ))) {
 			
-			final Charset charset = Charset.forName("ISO-8859-1");
+			//final Charset charset = Charset.forName("ISO-8859-1");
+			final Charset charset = Charset.forName("Cp1250");
+			
 			final ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-			int read = 0;
-			StringBuilder sb = new StringBuilder();
-			List<String> tokens = new ArrayList<>();
-			while((read = seekableByteChannel.read(buffer)) > 0) {
+			
+			final List<String> tokens = new ArrayList<>();
+			CharBuffer tokenBuffer = CharBuffer.allocate(255);
+			char c;
+			while(seekableByteChannel.read(buffer) > 0) {
 				buffer.flip();
 				final CharBuffer charBuffer = charset.decode(buffer);
-				
-				//charBuffer.flip();
-				while(charBuffer.remaining() > 0) {
-					char c = charBuffer.get();
+			
+				while(charBuffer.hasRemaining()) {
+					c = charBuffer.get();
 					if(c == rowDelimiter) {
+						tokenBuffer.flip();
+						tokens.add(tokenBuffer.toString());
+						tokenBuffer.clear();
 						println(tokens);
 						tokens.clear();
 					} else if (c == columnDelimiter) {
-						tokens.add(sb.toString());
-						sb = new StringBuilder();						
+						tokenBuffer.flip();
+						tokens.add(tokenBuffer.toString());
+						tokenBuffer.clear();
+
 					} else {
-						sb.append(c);
+						if(!tokenBuffer.hasRemaining()) {
+							final CharBuffer newTokenBuffer = CharBuffer.allocate(tokenBuffer.capacity() + 2);
+							tokenBuffer.flip();
+							tokenBuffer = newTokenBuffer.put(tokenBuffer);
+						}
+						tokenBuffer.put(c);
 					}
 				}
 				buffer.compact();
-				//charBuffer.clear();
 			}
 		}
 	}
 	
 	public static void println(List<String> tokens) {
-		System.out.println(tokens.toString());
+		System.out.println("size: " + tokens.size() + " " + tokens.toString());
 	}
 
 }
