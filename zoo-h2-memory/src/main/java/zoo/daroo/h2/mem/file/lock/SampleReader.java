dc55d22755aac4ddc1df174106b3998a31e8acf1
daru <dariusz.szablinski@gmail.com>
2012-11-12 19:48:22 +0100

    d

---
+++ zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/file/lock/SampleReader.java
@@ -0,0 +1,20 @@
+package zoo.daroo.h2.mem.file.lock;
+
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.StandardOpenOption;
+
+public class SampleReader {
+
+	public static void main(String[] args) throws Exception {
+
+		try (FileChannel channel = FileChannel.open(SampleWriter.file, StandardOpenOption.READ, StandardOpenOption.WRITE);
+				FileLock lock = channel.tryLock(0L, Long.MAX_VALUE, true)) {
+			if (lock != null) {
+				System.out.println("got lock: " + lock);
+			}
+		}
+
+	}
+
+}
---
+++ zoo-h2-memory/src/main/java/zoo/daroo/h2/mem/file/lock/SampleWriter.java
@@ -0,0 +1,23 @@
+package zoo.daroo.h2.mem.file.lock;
+
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SampleWriter {
+	
+	public static Path file = Paths.get("p:/temp/sample.file");
+	
+	public static void main(String[] args) throws Exception {
+		
+		FileWriter fw = new FileWriter(file.toFile(), false);
+		
+		for(int i = 0; i < 100000; i++) {
+			fw.write("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n");
+			fw.flush();
+		}
+		
+		fw.close();
+	}
+
+}
