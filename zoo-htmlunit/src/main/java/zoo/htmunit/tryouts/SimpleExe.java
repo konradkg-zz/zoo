386065e8f9c7d8582bcaba6bc9b2c7b1c290efa6
daru <dariusz.szablinski@gmail.com>
2013-03-05 21:01:36 +0100

    f

--- zoo-htmlunit/src/main/java/zoo/htmunit/tryouts/SimpleExe.java
+++ zoo-htmlunit/src/main/java/zoo/htmunit/tryouts/SimpleExe.java
@@ -1,5 +1,22 @@
 package zoo.htmunit.tryouts;
 
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
 public class SimpleExe {
+	
+	public static void main(String[] args) throws Exception {
+		
+		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_3_6);
+	    final HtmlPage page = webClient.getPage("http://www.gazeta.pl/0,0.html");
+	    
+	    final List<HtmlForm> forms = page.getForms();
+		
+	    forms.toString();
+	}
 
 }
